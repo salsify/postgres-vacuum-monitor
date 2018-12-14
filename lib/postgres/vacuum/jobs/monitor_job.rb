fadcae7a60003e1fc57b8a6aabda2321e9a0306b
Chris Flood <cflood@salsify.com>
2018-12-13 21:39:55 -0500

    Report per distinct db connection.

--- lib/postgres/vacuum/jobs/monitor_job.rb
+++ lib/postgres/vacuum/jobs/monitor_job.rb
@@ -7,24 +7,26 @@ module Postgres
         LONG_QUERIES = 'LongQueries'.freeze
 
         def perform(*)
-          ActiveRecord::Base.connection.execute(Postgres::Vacuum::Monitor::Query.long_running_queries).each do |row|
-            reporter_class.report_event(
-              LONG_QUERIES,
-              start_time: row['xact_start'],
-              running_time: row['seconds'],
-              application_name: row['application_name'],
-              query: row['query']
-            )
-          end
+          each_database_connection do |connection|
+            connection.execute(Postgres::Vacuum::Monitor::Query.long_running_queries).each do |row|
+              reporter_class.report_event(
+                LONG_QUERIES,
+                start_time: row['xact_start'],
+                running_time: row['seconds'],
+                application_name: row['application_name'],
+                query: row['query']
+              )
+            end
 
-          ActiveRecord::Base.connection.execute(Postgres::Vacuum::Monitor::Query.tables_eligible_vacuuming).each do |row|
-            reporter_class.report_event(
-              AUTOVACUUM_QUERY_EVENT,
-              table: row['relation'],
-              table_size: row['table_size'],
-              dead_tuples: row['dead_tuples'].to_i,
-              tuples_over_limit: row['dead_tuples'].to_i - row['autovacuum_vacuum_tuples'].to_i
-            )
+            connection.execute(Postgres::Vacuum::Monitor::Query.tables_eligible_vacuuming).each do |row|
+              reporter_class.report_event(
+                AUTOVACUUM_QUERY_EVENT,
+                table: row['relation'],
+                table_size: row['table_size'],
+                dead_tuples: row['dead_tuples'].to_i,
+                tuples_over_limit: row['dead_tuples'].to_i - row['autovacuum_vacuum_tuples'].to_i
+              )
+            end
           end
 
           true
@@ -37,6 +39,14 @@ module Postgres
           raise ConfigurationError.new('Missing or invalid report class name. Check your configuration') if @reporter_class_name.nil?
 
           @reporter_class_name
+        end
+
+        def each_database_connection
+          databases = Set.new
+          ActiveRecord::Base.connection_handler.connection_pools.map do |connection_pool|
+            db_name = connection_pool.spec.config[:database]
+            yield connection_pool.connection if databases.add?(db_name)
+          end
         end
 
         ConfigurationError = Class.new(StandardError)
